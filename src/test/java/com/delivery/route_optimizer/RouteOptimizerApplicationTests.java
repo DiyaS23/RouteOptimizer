61faf475cbfd529a0102a8e895f729d089a1a2b8
Diya <sabikhidiya@gmail.com>
2026-01-21 12:18:40 +0530

    Initial setup

---
+++ src/test/java/com/delivery/route_optimizer/RouteOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.route_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
