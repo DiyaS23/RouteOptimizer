8733cbc3ea31f05c6bf380cdae8d71177312d41d
Diya <sabikhidiya@gmail.com>
2026-01-21 22:59:50 +0530

    RedisConfig and Metrics done

--- src/main/java/com/delivery/route_optimizer/service/RouteService.java
+++ src/main/java/com/delivery/route_optimizer/service/RouteService.java
@@ -2,11 +2,13 @@ package com.delivery.route_optimizer.service;
 
 
 import com.delivery.route_optimizer.algorithm.*;
+import com.delivery.route_optimizer.metrics.RouteMetrics;
 import com.delivery.route_optimizer.model.Edge;
 import com.delivery.route_optimizer.model.Node;
 import com.delivery.route_optimizer.repository.EdgeRepository;
 import com.delivery.route_optimizer.repository.NodeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -17,9 +19,23 @@ public class RouteService {
 
     private final NodeRepository nodeRepository;
     private final EdgeRepository edgeRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final RouteMetrics metrics;
+
 
     public List<GraphNode> calculateShortestPath(String sourceCode, String targetCode) {
+        String cacheKey = "route:" + sourceCode + ":" + targetCode;
 
+        String cached = redisTemplate.opsForValue().get(cacheKey);
+        if (cached != null) {
+            metrics.cacheHits.increment();
+            return Arrays.stream(cached.split(","))
+                    .map(code -> new GraphNode(null, code))
+                    .toList();
+        }
+        else {
+            metrics.cacheMisses.increment();
+        }
         Node sourceNode = nodeRepository.findByCode(sourceCode)
                 .orElseThrow(() -> new RuntimeException("Source node not found"));
 
@@ -49,11 +65,21 @@ public class RouteService {
         }
 
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm();
-        return dijkstra.shortestPath(
+        List<GraphNode> path = dijkstra.shortestPath(
                 graph,
                 graphNodeMap.get(sourceNode.getId()),
                 graphNodeMap.get(targetNode.getId())
         );
+
+        String value = path.stream()
+                .map(GraphNode::getCode)
+                .reduce((a, b) -> a + "," + b)
+                .orElse("");
+
+        redisTemplate.opsForValue()
+                .set(cacheKey, value, java.time.Duration.ofMinutes(5));
+
+        return path;
     }
 }
 
