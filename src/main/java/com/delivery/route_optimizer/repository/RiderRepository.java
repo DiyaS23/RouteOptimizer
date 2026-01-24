2d2bd74d0dd420b8a5c2ff72ab6721c939b07fd9
Diya <sabikhidiya@gmail.com>
2026-01-24 14:36:34 +0530

    Removed duplicated files

--- src/main/java/com/delivery/route_optimizer/repository/RiderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delivery.route_optimizer.repository;
-
-import com.delivery.route_optimizer.model.Rider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RiderRepository extends JpaRepository<Rider, Long> {
-}
-
